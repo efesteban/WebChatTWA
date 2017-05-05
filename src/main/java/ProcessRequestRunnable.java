49006d4a4003599a471e542a4c481e1562c1513a
FrancoEsteban <fezdoat_92@hotmail.com>
2017-05-05 18:16:36 -0300

    Conexion GitHub

--- src/main/java/ProcessRequestRunnable.java
+++ src/main/java/ProcessRequestRunnable.java
@@ -2,6 +2,7 @@ import com.google.common.base.Stopwatch;
 import com.google.common.collect.Lists;
 import com.google.common.io.Resources;
 
+import com.sun.tools.doclets.internal.toolkit.util.DocFinder;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.io.LineIterator;
 import org.apache.commons.lang3.RandomStringUtils;
@@ -11,11 +12,20 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.URL;
 import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * @author Diego Urrutia Astorga <durrutia@ucn.cl>
@@ -38,14 +48,29 @@ public class ProcessRequestRunnable implements Runnable {
      */
     private Socket socket;
 
+    /**
+     * Atributos del cliente
+     */
+    private int colorFont;
+    private String avatar;
+    private String apodo;
+
+
+
 
     /**
      * Constructor
      *
      * @param socket
      */
-    public ProcessRequestRunnable(final Socket socket) {
+    public ProcessRequestRunnable(final Socket socket, final int indice) {
         this.socket = socket;
+        if(indice <10) {
+            this.apodo = "ChatUsser00" + indice;
+        }else
+        {
+            this.apodo = "ChatUsser0" + indice;
+        }
     }
 
 
@@ -68,26 +93,29 @@ public class ProcessRequestRunnable implements Runnable {
 
         log.debug("Connection from {} in port {}.", socket.getInetAddress(), socket.getPort());
 
-        // A dormir!
-        try {
-            Thread.sleep(RandomUtils.nextInt(100, 500));
-        } catch (InterruptedException e) {
-            log.error("Error in sleeping", e);
-        }
 
-        try {
-            processRequest(this.socket);
-        } catch (Exception ex) {
-            log.error("Error processing request", ex);
-        } finally {
+            // A dormir!
             try {
-                socket.close();
-            } catch (IOException e) {
-                // Nothing here
+                Thread.sleep(RandomUtils.nextInt(100, 500));
+            } catch (InterruptedException e) {
+                log.error("Error in sleeping", e);
             }
-        }
 
-        log.debug("Request timex: {}.", stopWatch);
+            try {
+                processRequest(this.socket, this.apodo);
+            } catch (Exception ex) {
+                log.error("Error processing request", ex);
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    // Nothing here
+                }
+            }
+
+            log.debug("Request timex: {}.", stopWatch);
+
+        //Remover el cliente de la lista
 
     }
 
@@ -96,7 +124,10 @@ public class ProcessRequestRunnable implements Runnable {
      *
      * @param socket
      */
-    private static void processRequest(final Socket socket) throws IOException {
+
+
+
+    private static void processRequest(final Socket socket, final String apodo) throws IOException {
 
         // Iterador de la peticion
         final LineIterator lineIterator = IOUtils.lineIterator(socket.getInputStream(), Charset.defaultCharset());
@@ -123,6 +154,9 @@ public class ProcessRequestRunnable implements Runnable {
         final String uri = request3[1];
         final String version = request3[2];
 
+        log.warn(" verbo: {}", verbo);
+
+
         // Deteccion de version
         if (!StringUtils.equals("HTTP/1.1", version)) {
             log.warn("Wrong version: {}", version);
@@ -130,7 +164,7 @@ public class ProcessRequestRunnable implements Runnable {
 
         // Si la peticion incluye /chat
         if (StringUtils.startsWith(uri, "/chat")) {
-            writeChat(outputStream, verbo, uri);
+            writeChat(outputStream, verbo, uri, apodo);
         } else {
             // HTTP Body
             writeBody(outputStream, request);
@@ -141,20 +175,31 @@ public class ProcessRequestRunnable implements Runnable {
 
     }
 
+
     /**
      * @param outputStream
      * @param verbo
      * @param uri
      */
-    private static void writeChat(final OutputStream outputStream, final String verbo, final String uri) throws IOException {
+    private static void writeChat(final OutputStream outputStream, final String verbo, final String uri, final String apodo) throws IOException {
 
-        if (StringUtils.contains(uri, "chat?msgText=")) {
-            final String msg = StringUtils.substringAfter(uri, "chat?msgText=");
-            log.debug("Msg to include: {}", msg);
+        if (StringUtils.contains(uri, "/chat")) {
 
-            // Sincronizacion
-            synchronized (chats) {
-                chats.add(msg);
+            Date date = new Date();
+            String strDateFormat = "hh:mm:ss";
+            DateFormat dateFormat = new SimpleDateFormat(strDateFormat);
+            String formattedDate= dateFormat.format(date);
+
+            if( !StringUtils.substringAfter(uri, "chat?msgText=").equals("")) {
+                String msg = formattedDate + " " + apodo + ":" + StringUtils.substringAfter(uri, "chat?msgText=");
+
+
+                log.debug("Msg to include: {}", msg);
+
+                // Sincronizacion
+                synchronized (chats) {
+                    chats.add(msg);
+                }
             }
         }
 
@@ -172,6 +217,8 @@ public class ProcessRequestRunnable implements Runnable {
             }
         }
 
+
+
         // Contenido completo
         final String content = readFile("index.html");
 
@@ -181,6 +228,37 @@ public class ProcessRequestRunnable implements Runnable {
         IOUtils.write(contentChat + "\r\n", outputStream, Charset.defaultCharset());
 
     }
+
+    public static void recibeMsg(final Socket socket) throws  IOException{
+
+
+        final  OutputStream outputStream = IOUtils.buffer(socket.getOutputStream());
+
+        // Listado completo de chat
+        final StringBuffer sb = new StringBuffer();
+
+        // Linea de chat
+        final String chatline = readFile("chatline.html");
+
+        // Siempre el mismo comportamiento
+        synchronized (chats) {
+            for (String line : chats) {
+                sb.append(StringUtils.replace(chatline, "CONTENT", line));
+                sb.append("\r\n");
+            }
+        }
+
+
+
+        // Contenido completo
+        final String content = readFile("index.html");
+
+        final String contentChat = StringUtils.replace(content, "<!-- CHAT_CONTENT-->", sb.toString());
+
+        // Envio el contenido
+        IOUtils.write(contentChat + "\r\n", outputStream, Charset.defaultCharset());
+    }
+
 
     /**
      * @param filename
@@ -217,6 +295,9 @@ public class ProcessRequestRunnable implements Runnable {
 
         String request = null;
         int n = 0;
+
+        log.debug("entramos al get Request");
+
 
         while (lineIterator.hasNext()) {
 
--- src/main/java/WebServerThreads.java
+++ src/main/java/WebServerThreads.java
@@ -1,9 +1,14 @@
+import com.google.common.collect.Lists;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.BufferedInputStream;
+import java.io.DataInputStream;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -11,7 +16,7 @@ import java.util.concurrent.Executors;
  * @author Diego Urrutia Astorga <durrutia@ucn.cl>
  * @version 20170330131600
  */
-public class WebServerThreads {
+public class WebServerThreads extends Thread {
 
     /**
      * Logger de la clase
@@ -21,13 +26,21 @@ public class WebServerThreads {
     /**
      * Puerto de escucha
      */
-    private static final Integer PORT = 9000;
+    private static final Integer PORT = 9090;
+
+
+    /**
+     * Lista de clientes conectados
+     */
+    private static final List<String> ipClientes = Lists.newArrayList();
+    private static final List<Socket> listaClientes = Lists.newArrayList();
 
     /**
      * Inicio del programa.
      *
      * @param args
      */
+
     public static void main(String[] args) throws IOException {
 
         log.debug("Starting ..");
@@ -44,13 +57,46 @@ public class WebServerThreads {
 
             // 1 socket por peticion
             final Socket socket = serverSocket.accept();
+            String ipCliente = socket.getInetAddress().getHostAddress();
 
-            // Al executor ..
-            final Runnable runnable = new ProcessRequestRunnable(socket);
-            executor.execute(runnable);
+            //el cliente no debe existir
+            if(!ipClientes.contains(ipCliente)) {
 
-            log.debug("Connection from {} in port {}.", socket.getInetAddress(), socket.getPort());
 
+                // Al executor ..
+                final Runnable runnable = new ProcessRequestRunnable(socket, listaClientes.size() + 1);
+                executor.execute(runnable);
+
+                //Agregamos la ip y el cliente a sus listas correspondientes
+                PrintStream ps = new PrintStream(socket.getOutputStream());
+                ipClientes.add(ipCliente);
+                listaClientes.add(socket);
+
+                log.debug("Connection from {} in port {}.", socket.getInetAddress(), socket.getPort());
+            }else
+            {
+                //el cliente ya existe
+                int i = 0;
+                //buscamos y capturamos al cliente
+                while(!ipClientes.get(i).equals(ipCliente)){
+                    i ++;
+                }
+
+                // Al executor ..
+                //listaClientes.get(i).connect(serverSocket.getLocalSocketAddress());
+
+                final Runnable runnable = new ProcessRequestRunnable(socket, i + 1);
+                executor.execute(runnable);
+
+
+                //log.debug("El usuario de IP {} volvió", ipCliente);
+                //log.debug("Connection from {} in port {}.", socket.getInetAddress(), socket.getPort());
+            }
+
+            for (int i = 0; i < listaClientes.size(); i ++){
+
+            }
         }
     }
+
 }
